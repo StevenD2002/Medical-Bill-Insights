2cc98826fe430966ff1bb27e305f8d4d170642af
Steven Dew <stevendew02@gmail.com>
2024-03-22 22:24:03 -0500

    📦 FEAT: wip - start llm docker process

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+// need to parse the data from out.txt into a json object
+package main
+
+import "fmt"
+
+func main() {
+	// read the file
+	// parse the data
+	// create a json object
+	// print the json object
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/StevenD2002/Medical-Bill-Insights
+
+go 1.21.6
