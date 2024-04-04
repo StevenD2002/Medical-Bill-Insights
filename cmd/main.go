7ade3be816144871755cb8a8cb7fcef12225f6b2
Steven Dew <stevendew02@gmail.com>
2024-04-03 22:52:57 -0500

    📦 FEAT: add templ + add notebook with bill research

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,16 @@
-// need to parse the data from out.txt into a json object
 package main
 
-import "fmt"
+import (
+	"context"
+	"net/http"
+
+	"github.com/StevenD2002/Medical-Bill-Insights/web/template/partials"
+)
 
 func main() {
-	// read the file
-	// parse the data
-	// create a json object
-	// print the json object
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		component := partials.Hello("Steven")
+		component.Render(context.Background(), w)
+	})
+	http.ListenAndServe(":8080", nil)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/StevenD2002/Medical-Bill-Insights
 
 go 1.21.6
+
+require github.com/a-h/templ v0.2.648 // indirect
